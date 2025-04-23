dbc6241bf7f2ceeb34905bfeb77469624ff57533
MJMrigg <riggmatthew20@outlook.com>
2025-04-23 06:25:13 -0400

    Refactored

--- Server/server.js
+++ Server/server.js
@@ -33,14 +33,6 @@ app.post("/createAccount", async(request, response) => {
   await client.connect();
   //Try to insert account data
   try{
-    /*const expectedParams = 1;
-    const actualParams = process.argv.length - 2;
-    if(actualParams < expectedParams){
-      throw new Error(
-        `Invalid number of parameters. Expected ${expectedParams}, got ${actualParams}.`
-      );
-    }*/
-
     //Connect to the proper database and collection
     var dataBase = "SSE_MobileSecurityGame";
     var dbCollection = "Testing"
@@ -90,14 +82,6 @@ app.post("/login", async(request, response) =>{
   await client.connect();
   //Try to retrieve account data
   try{
-    /*const expectedParams = 1;
-    const actualParams = process.argv.length - 2;
-    if(actualParams < expectedParams){
-      throw new Error(
-        `Invalid number of parameters. Expected ${expectedParams}, got ${actualParams}.`
-      );
-    }*/
-
     //Connect to the proper database and collection
     var dataBase = "SSE_MobileSecurityGame";
     var dbCollection = "Testing"
@@ -145,14 +129,6 @@ app.post("/update", async(request,response) =>{
   await client.connect();
   //Try to retrieve account data
   try{
-    /*const expectedParams = 1;
-    const actualParams = process.argv.length - 2;
-    if(actualParams < expectedParams){
-      throw new Error(
-        `Invalid number of parameters. Expected ${expectedParams}, got ${actualParams}.`
-      );
-    }*/
-
     //Connect to the proper database and collection
     var dataBase = "SSE_MobileSecurityGame";
     var dbCollection = "Testing"
@@ -201,14 +177,6 @@ app.post("/checkUsername",async(request,response)=>{
   await client.connect();
   //Try to retrieve account data
   try{
-    /*const expectedParams = 1;
-    const actualParams = process.argv.length - 2;
-    if(actualParams < expectedParams){
-      throw new Error(
-        `Invalid number of parameters. Expected ${expectedParams}, got ${actualParams}.`
-      );
-    }*/
-
     //Connect to the proper database and collection
     var dataBase = "SSE_MobileSecurityGame";
     var dbCollection = "Testing"
@@ -245,14 +213,6 @@ app.post("/checkEmail",async(request,response)=>{
   await client.connect();
   //Try to retrieve account data
   try{
-    /*const expectedParams = 1;
-    const actualParams = process.argv.length - 2;
-    if(actualParams < expectedParams){
-      throw new Error(
-        `Invalid number of parameters. Expected ${expectedParams}, got ${actualParams}.`
-      );
-    }*/
-
     //Connect to the proper database and collection
     var dataBase = "SSE_MobileSecurityGame";
     var dbCollection = "Testing"
@@ -288,14 +248,6 @@ app.post("/getScoreBoard", async(request,response) =>{
   await client.connect();
   //Try to retrieve account data
   try{
-    /*const expectedParams = 1;
-    const actualParams = process.argv.length - 2;
-    if(actualParams < expectedParams){
-      throw new Error(
-        `Invalid number of parameters. Expected ${expectedParams}, got ${actualParams}.`
-      );
-    }*/
-
     //Connect to the proper database and collection
     var dataBase = "SSE_MobileSecurityGame";
     var dbCollection = "Testing"
@@ -326,51 +278,3 @@ app.post("/getScoreBoard", async(request,response) =>{
 app.listen(3000, () => {
   console.log('Server listening on http://localhost:3000');
 });
-
-/*
-(async () => {
-  try {
-    const expectedParams = 1;
-    const actualParams = process.argv.length - 2;
-
-    if (actualParams < expectedParams) {
-      throw new Error(
-        `Invalid number of parameters. Expected ${expectedParams}, got ${actualParams}.`
-      );
-    }
-
-    const filePath = process.argv[2];
-
-    // Connect to MongoDB and check connection
-    await client.connect();
-    console.log("Connected to MongoDB successfully");
-
-    const db = client.db("SSE_MobileSecurityGame");
-    const collection = db.collection("Testing");
-
-    // Read and process the file
-    const fileContent = fs.readFileSync(filePath, "utf-8").trim();
-    const jsonData = JSON.parse(fileContent);
-
-    console.log('File content parsed:', jsonData)
-    console.log('File content length: ${JSON.stringify(jsonData).length}')
-
-    const document = {
-      content: jsonData,
-      insertedAt: new Date()
-    };
-
-    // Insert the document into MongoDB
-    const result = await collection.insertOne(document);
-
-    if (result.acknowledged) {
-      console.log(`Document inserted with _id: ${result.insertedId}`);
-    } else {
-      console.log("Failed to insert the document");
-    }
-  } catch (err) {
-    console.error(`[Error] ${err}`);
-  } finally {
-    await client.close();
-  }
-})();*/
